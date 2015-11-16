6b0fa2e3fe1eba9dd8fc2675fc99c19887178646
Stefano Balietti <futur.dorko@gmail.com>
2015-11-16 08:20:17 +0100

    updated_stager_ex

--- stager_exercise.js
+++ stager_exercise.js
@@ -7,12 +7,8 @@ var ngc = require('./nodegame-client/index.js');
 var Stager = ngc.Stager;
 var stager = new Stager();
 
-
 // Alternatively.
 // var stager = ngc.getStager();
-
-// Result variable.
-var result;
 
 // Utility functions.
 
@@ -72,11 +68,12 @@ function testPositions(stager, len, debug) {
 // To test your sequence use testPositions(stager, number of tests, verbose)
 
 // 1. Three sequential stages named 'stage 1', 'stage 2', 'stage 3'.
-stager.stage('stage 1');
-stager.stage('stage 2');
-stager.stage('stage 3');
 
-testPositions(stager, 1, true);
+// stager.stage('stage 1');
+// stager.stage('stage 2');
+// stager.stage('stage 3');
+// 
+// testPositions(stager, 1, true);
 
 // 2. Like 1., but 'stage 2' contains two nested steps.
 
@@ -88,4 +85,87 @@ testPositions(stager, 1, true);
 
 // 6. Run stage 3 either first or last.
 
-// 7. Reproduce the sequence of the car sharing game.
+// Advanced exercises:
+
+// 7. Extend the callback of the step of 'stage 1'.
+// Make it print the stage position in sequence.
+// Hint: this.getCurrentGameStage()
+// Question: what exactly is the `this` object here? (more on "this" below)
+// Question: what is the difference between:
+// stager.extendStep and stager.extendStage? Why?
+
+// 8. Store the 'state' of the stager to a variable.
+// Try to add a new stage to the stager. What happens?
+
+// 9. Use the previously saved 'state' of the stager to
+// initialize a new stager object. Check its sequence.
+
+// 10. Extend again the step of 'stage 1'.
+// Add a callback that calls the previous callback and 
+// then adds an item (object) to the `memory` database.
+// Hint: this.getCurrentStepObj()
+// Hint: the this.memory object is an instance of NDDB
+// http://nodegame.github.io/NDDB/
+
+// 11. Set an `init` function for the stager.
+// In the init function add an event listener 
+// on 'insert' of new items in the memory database.
+// When an item is inserted it gets modified by adding your name in a property.
+// Repeat step 10, and check what happens when you insert an item.
+
+// 12. The this.pl (PlayerList) object 
+// contains the list of currently connected players.
+// Check the are no connected players at the moment. Hint .size()
+
+// 13. Extend 'stage 2', and add an object named globals. 
+// The object will be available in this.globals when executing any step 
+// of stage 2. Check that.
+// Question: If, in addition, you extend also one step 
+// of 'stage 2' with another globals property, how does the content of
+// `this.globals` changes through the stages and steps?
+
+// Go free-style.
+
+// 14. Uncomment the lines below.
+
+stager.stage('stage 1');
+stager.stage('stage 2');
+stager.stage('stage 3');
+
+stager.setDefaultStepRule(ngc.stepRules.WAIT);
+sstate = stager.getState();
+node = ngc.getClient();
+node.verbosity = 1;
+node.setup('plot', sstate);
+node.createPlayer({ id: 'testid' });
+node.game.start();
+
+// Pay attention to the line:
+// stager.setDefaultStepRule(ngc.stepRules.WAIT);
+
+// Step rules determine what to do next when a step is _done_.
+// ngc.stepRules.WAIT just says to wait.
+
+// Comment the line: 
+// stager.setDefaultStepRule(ngc.stepRules.WAIT);
+// and add the line:
+// stager.setDefaultStepRule(ngc.stepRules.SOLO);
+
+// What do you think will happen? Why?
+// What is the default callback? 
+// Hint: console.log(stager.getDefaultCallback().toString());
+// What is node.done doing?
+
+// Check out:
+// https://github.com/nodeGame/nodegame-client/blob/master/lib/modules/stepRules.js
+// for more step rules.
+
+// Important! Different step rules can be applied to
+// different stages and steps to let a player advance through some steps,
+// but wait for other player to be sync with him/her in others.
+
+// Bonus Question: What is the difference between node.game and `this.game`
+// when a step callback is executed?
+
+// Bonus Question: node.player is an important object. What does it contain?
+
